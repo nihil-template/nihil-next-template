f6c9bc2921f6e91cb0404cf80f27036bcc3ef2fc
NIHILncunia <nihil_ncunia@naver.com>
2021-08-15 07:51:37 +0900

    2021 0815 템플릿 업데이트

--- .eslintrc.js
+++ .eslintrc.js
@@ -60,10 +60,18 @@ module.exports = {
       exports: 'never',
     }, ],
     'jsx-quotes': [ 'error', 'prefer-single', ],
-    'linebreak-style': 'off',
+    'linebreak-style': [ 'warn', 'windows', ],
+    'max-len': 'off',
+    'lines-between-class-members': 'off',
+    'no-underscore-dangle': 'off',
 
     // 임포트 규칙
-    'import/extensions': [ 'error', 'never', { ignorePackages: true, }, ],
+    'import/extensions': [ 'error', 'never', {
+      ignorePackages: true,
+      pattern: {
+        json: 'always',
+      },
+    }, ],
     'import/no-extraneous-dependencies': 'off',
     'import/no-unresolved': 'off',
     'import/prefer-default-export': 'off',
@@ -81,6 +89,7 @@ module.exports = {
     ],
     'react/require-default-props': 'off',
     'react/jsx-one-expression-per-line': 'off',
+    'react/display-name': 'off',
 
     // 리액트 훅스 규칙
     'react-hooks/exhaustive-deps': 'off',
